122cd86a551717ba500b53f409b18de24f2ddbe5
GOKUMUI12 <ammar.iftikhar0002@gmail.com>
2024-04-07 01:34:54 +0500

    Second Commit

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// * WEB700 – Assignment 06* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this* assignment has been copied manually or electronically from any other source (including web sites) or* distributed to other students.**
+//  Name: Muhammad Shahman Iqbal Student ID: 136212230 Date: 06/04/2024** 
+// Online  (Cyclic) Link: ______
+
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
